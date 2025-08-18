d52f303d1daa77fe2d2e74f3646e86d8db5aa8b1
Quri <qurikzqurikz@gmail.com>
2025-08-18 17:53:03 +0500

    Add files via upload

---
+++ teleport.lua
@@ -0,0 +1,32 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+if game.PlaceId ~= 16447934574 then return end
+if _G.SL then return end
+_G.SL = true
+
+local ts = game:GetService("TeleportService")
+
+local portals = {
+    PortalEliteClanLeague = 16452714764,
+    PortalElite = 16876418704,
+    PortalPro = 16452698027,
+    PortalAcademy = 16452695324,
+    PortalJunior = 16452693191,
+    PortalQuests = 81844361462023,
+    PortalStreet = 105531756926941,
+    PortalFutsal = 16452721151,
+    PortalCup = 16453243555,
+    Portal1vs1 = 18738477182,
+    PortalWorldElimination = 110385211492799,
+    PortalBeach = 16452699830,
+    PortalProCup = 138792551842718
+}
+
+function tp(pn, pd)
+    workspace.Lobby.Portals[pn].Touched:Connect(function()
+        ts:TeleportAsync(pd)
+    end)
+end
+
+for pn, pd in pairs(portals) do
+    tp(pn, pd)
+end
